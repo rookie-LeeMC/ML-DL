ba01617d3eed1c206fc5667ff301f900512fecb8
rookie <1967139340@qq.com>
2018-09-03 11:53:11 +0800

    lmc

--- pythonDemo/functional_prog.py
+++ pythonDemo/functional_prog.py
@@ -42,23 +42,36 @@
 # l1 = ['lmc', 'adaM', 'LISA']
 # print(list(map(normalize, l1)))
 
-from functools import reduce
+# from functools import reduce
+#
+#
+# def str2float(s):
+#     def fn(x, y):
+#         return 10 * x + y
+#
+#     def char2num(s):
+#         digit = {'0': 0, '1': 1, '2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9}
+#         return digit[s]
+#
+#     return reduce(fn, map(char2num, s.replace('.', '')))
+#
+#
+# s = '1234.5678'
+#
+# if s.find('.') != -1:
+#     print(str2float(s) / pow(10, (len(s) - s.find('.') - 1)))
+# else:
+#     print(str2float(s))
+
+# def is_palindrome(n):
+#     s1 = str(n)
+#     s2 = s1[::-1]
+#
+#     if s1 == s2:
+#         return True
+#     else:
+#         return False
+# l = [1, 909, 12321, 2134]
+# print(list(filter(is_palindrome, l)))
 
 
-def str2float(s):
-    def fn(x, y):
-        return 10 * x + y
-
-    def char2num(s):
-        digit = {'0': 0, '1': 1, '2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9}
-        return digit[s]
-
-    return reduce(fn, map(char2num, s.replace('.', '')))
-
-
-s = '1234.5678'
-
-if s.find('.') != -1:
-    print(str2float(s) / pow(10, (len(s) - s.find('.') - 1)))
-else:
-    print(str2float(s))
